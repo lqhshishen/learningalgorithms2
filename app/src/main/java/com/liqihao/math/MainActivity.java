f87166d39252f84220724b0ec9abfa7e3615be4b
liqihao <543376397@qq.com>
2018-02-28 17:54:33 +0800

    3题easy

--- app/src/main/java/com/liqihao/math/MainActivity.java
+++ app/src/main/java/com/liqihao/math/MainActivity.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int [] arr = new int []{1,1,2};
-        Log.e("test", String.valueOf(reverse(1534236469)));
+        isPalindrome(-2147483648);
+//        Log.e("test", String.valueOf(isPalindrome(-2147483648)));
 
 
 //        quickSort(arr,0,arr.length - 1);
@@ -90,27 +90,27 @@ public class MainActivity extends AppCompatActivity {
         Log.e("Max_Heapify",Arrays.toString(arr));
     }
 
-    public int removeDuplicates(int[] nums) {
-        List<Integer> a = new ArrayList<>();
-        int [] x = new int[nums.length - 1];
-        for (int i = 0; i < nums.length; i++) {
-            boolean b = true;
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] == nums[j])
-                    b = false;
-            }
-            if(b){
-                a.add(i);
-            }
-        }
-        for (int i = 0; i < a.size(); i++) {
-            Log.e("test1",a.get(i).toString());
-        }
-        for (int i = 0; i < nums.length; i++) {
-            Log.e("test2", String.valueOf(nums[i]));
-        }
-        return a.size();
-    }
+//    public int removeDuplicates(int[] nums) {
+//        List<Integer> a = new ArrayList<>();
+//        int [] x = new int[nums.length - 1];
+//        for (int i = 0; i < nums.length; i++) {
+//            boolean b = true;
+//            for (int j = i+1; j < nums.length; j++) {
+//                if (nums[i] == nums[j])
+//                    b = false;
+//            }
+//            if(b){
+//                a.add(i);
+//            }
+//        }
+//        for (int i = 0; i < a.size(); i++) {
+//            Log.e("test1",a.get(i).toString());
+//        }
+//        for (int i = 0; i < nums.length; i++) {
+//            Log.e("test2", String.valueOf(nums[i]));
+//        }
+//        return a.size();
+//    }
 
     public int[] twoSum(int[] nums, int target) {
         int result[];
@@ -158,4 +158,90 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
+    //Palindrome Number
+    public boolean isPalindrome(int x) {
+        int tempX = x;
+        List<Integer>number = new ArrayList<>();
+        if (x < 0) {
+            return false;
+        }
+        while (tempX != 0){
+            number.add(tempX % 10);
+            tempX /= 10;
+        }
+        int i = 0,j = number.size() -1;
+        while (i < j) {
+            if (!Objects.equals(number.get(i), number.get(j)))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    //Roman to Integer
+//    public int romanToInt(String s) {
+//        int result = 0;
+//        if (s.contains("M"))
+//        return result;
+//    }
+//    public String longestCommonPrefix(String[] strs) {
+//        if (strs.length == 0)
+//            return "";
+//        for (int i = 0; i < strs.length; i++) {
+//            char a = strs[i].charAt()
+//        }
+//    }
+    //Valid Parentheses
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() == 1 || s.isEmpty())
+            return false;
+        for (char temp : s.toCharArray()) {
+            if (temp == '(')
+                stack.push(')');
+            else if (temp == '{')
+                stack.push('}');
+            else if ((temp == '['))
+                stack.push(']');
+            else if (temp != stack.pop() || stack.isEmpty())
+                return false;
+        }
+        return true;
+    }
+    //Merge Two Sorted Lists
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+    }
+
+//    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//
+//    }
+
+//    public int removeDuplicates(int[] nums) {
+//        int temp = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            boolean b = true;
+//            for (int j = i+1; j < nums.length; j++) {
+//                if (nums[i] == nums[j])
+//                    b = false;
+//            }
+//            if(b ){
+//            }
+//        }
+//        return list.length;
+//    }
+    //Remove Element
+    public int removeElement(int[] nums, int val) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[result] = nums[i];
+                result++;
+            }
+        }
+        return result;
+    }
 }
